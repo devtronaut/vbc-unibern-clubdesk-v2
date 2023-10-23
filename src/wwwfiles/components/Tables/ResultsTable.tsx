fd1815b8823c9f10dd3a0bbae5fff054f8bab690
Fabian Diemand <fabian.diemand@hotmail.com>
2023-10-23 20:51:54 +0000

    bugfix(gray-rows): fix gray rows in table by creating empty tds to fill until max played sets

--- src/wwwfiles/components/Tables/ResultsTable.tsx
+++ src/wwwfiles/components/Tables/ResultsTable.tsx
@@ -10,12 +10,17 @@ import { Toast } from '../Toast/Toast';
 export const ResultsTable = (teamProps: TeamProps) => {
   const [isLoading, setLoading] = useState(true);
   const [results, setResults] = useState({} as ResultPerTeamSchema);
+  const [maxPlayedSets, setMaxPlayedSets] = useState(0);
 
   useEffect(() => {
     ResultsService.fetchResults(teamProps.teamId)
       .then(results => {
         setResults(results);
         setLoading(false);
+
+        // Find out how many sets have been played at max from all the results
+        const maxSets = Math.max(...results.results.map(r => r.winner.sets.length));
+        setMaxPlayedSets(maxSets)
       })
       .catch(_ => {
         setLoading(false);
@@ -43,23 +48,33 @@ export const ResultsTable = (teamProps: TeamProps) => {
       </thead>
         {
           results.results.map((result: ResultsSchema, index: number) => {
-            return (<TableRow key={index} {...result} />)
+            return (<TableRow key={index} {...result} maxPlayedSets={maxPlayedSets}/>)
           })
         }
     </table>
   );
 }
 
-const TableRow = (props: ResultsSchema) => {
+const TableRow = (props: ResultsSchema & {maxPlayedSets: number}) => {
   const [result, setResult] = useState({} as ResultsSchema);
   const [shortDate, setShortDate] = useState('');
   const [loser, setLoser] = useState({} as ResultTeamSchema);
+  const [loserSets, setLoserSets] = useState([] as number[]);
   const [winner, setWinner] = useState({} as ResultTeamSchema);
+  const [winnerSets, setWinnerSets] = useState([] as number[]);
 
   useEffect(() => {
+    const winnerSets = props.winner.sets;
+    const loserSets = props.loser.sets;
+    const setDifference = props.maxPlayedSets - winnerSets.length;
+
     setResult(props);
     setLoser(props.loser);
     setWinner(props.winner);
+    
+    // Fill the sets array to the maximum number of sets played (to have an equal amount of cells in each row)
+    setWinnerSets(winnerSets.concat(new Array(setDifference).fill(null, 0, setDifference)));
+    setLoserSets(loserSets.concat(new Array(setDifference).fill(null, 0, setDifference)));
 
     const [, short,] = DateTransformer.transformDate(props.dateUtc);
     setShortDate(short);
@@ -72,12 +87,12 @@ const TableRow = (props: ResultsSchema) => {
         <td rowSpan={2} className='tw-text-center tw-py-1 phone:tw-hidden'>{result.mode}</td>
         <td className='tw-text-center tw-pt-1 tw-whitespace-nowrap tw-overflow-hidden tw-text-ellipsis'><strong>{winner.caption}</strong></td>
         <td className='tw-text-center tw-pt-1'><strong>{winner.setsWon}</strong></td>
-        {winner.sets?.map((set, index) => { return <td className='tw-text-left tw-pt-1 phone:tw-hidden' key={index}>{set > loser.sets[index] ? (<strong>{set}</strong>) : (<span className='tw-text-neutral-600'>{set}</span>)}</td> })}
+        {winnerSets.map((set, index) => { return <td className='tw-text-left tw-pt-1 phone:tw-hidden' key={index}>{set > loser.sets[index] ? (<strong>{set}</strong>) : (<span className='tw-text-neutral-600'>{set}</span>)}</td> })}
       </tr>
       <tr className='tw-border-0 tw-border-b-2 tw-border-solid tw-border-slate-200 hover:tw-bg-slate-100'>
         <td className='tw-text-center tw-pb-1 tw-whitespace-nowrap tw-overflow-hidden tw-text-ellipsis'>{loser.caption}</td>
         <td className='tw-text-center tw-pb-1'>{loser.setsWon}</td>
-        {loser.sets?.map((set, index) => { return <td className='tw-text-left tw-pt-1 phone:tw-hidden' key={index}>{set > winner.sets[index] ? (<strong>{set}</strong>) : (<span className='tw-text-neutral-600'>{set}</span>)}</td> })}
+        {loserSets.map((set, index) => { return <td className='tw-text-left tw-pt-1 phone:tw-hidden' key={index}>{set > winner.sets[index] ? (<strong>{set}</strong>) : (<span className='tw-text-neutral-600'>{set}</span>)}</td> })}
       </tr>
     </tbody>
   )
