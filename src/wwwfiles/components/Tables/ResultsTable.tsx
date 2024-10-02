f65e094398079655970103aedcab580d067607e7
Fabian Diemand <fabian.diemand@hotmail.com>
2024-10-02 16:00:18 +0200

    fix(results-fetch): refactor fetching to use hooks

--- src/wwwfiles/components/Tables/ResultsTable.tsx
+++ src/wwwfiles/components/Tables/ResultsTable.tsx
@@ -1,61 +1,75 @@
 import { useEffect, useState } from 'react';
 import { TeamProps } from '../../App';
-import { ResultPerTeamSchema, ResultsSchema, ResultTeamSchema } from '../../common/types/ResultsByTeam.type';
+import {
+  ResultPerTeamSchema,
+  ResultsSchema,
+  ResultTeamSchema,
+} from '../../common/types/ResultsByTeam.type';
 import DateTransformer from '../../common/utils/transform/DateTransformer';
-import ResultsService from '../../services/results/ResultsService';
 import { Spinner } from '../Loading/Spinner';
 import { Toast } from '../Toast/Toast';
+import { useTablesApi } from '../../common/hooks/useFetch';
 
+const useResultsApi = (
+  teamId: number
+): [boolean, ResultPerTeamSchema, number, boolean] => {
+  const [loading, data, error] = useTablesApi<ResultPerTeamSchema>(
+    'results-service',
+    teamId
+  );
+
+  const maxSets = loading || error
+    ? 0
+    : Math.max(...data.results.map((r) => r.winner.sets.length));
+
+  return [loading, data, maxSets, error];
+};
 
 export const ResultsTable = (teamProps: TeamProps) => {
-  const [isLoading, setLoading] = useState(true);
-  const [results, setResults] = useState({} as ResultPerTeamSchema);
-  const [maxPlayedSets, setMaxPlayedSets] = useState(0);
+  const [loading, results, maxPlayedSets, error] = useResultsApi(
+    teamProps.teamId
+  );
 
-  useEffect(() => {
-    ResultsService.fetchResults(teamProps.teamId)
-      .then(results => {
-        setResults(results);
-        setLoading(false);
+  if (error) {
+    console.error('Error while fetching results.');
+    return (
+      <Toast text="Beim Laden der Resultate ist ein Fehler aufgetreten." />
+    );
+  }
 
-        // Find out how many sets have been played at max from all the results
-        const maxSets = Math.max(...results.results.map(r => r.winner.sets.length));
-        setMaxPlayedSets(maxSets)
-      })
-      .catch(_ => {
-        setLoading(false);
-      })
-  }, []);
-
-  if (isLoading) {
-    return (<Spinner text='Lade Resultate ...' />);
+  if (loading) {
+    return <Spinner text="Lade Resultate ..." />;
   }
 
   if (!results.results || results.results.length === 0) {
-    return (<Toast text='Für deine Mannschaft sind noch keine Resultate vorhanden.' />)
+    return (
+      <Toast text="Für deine Mannschaft sind noch keine Resultate vorhanden." />
+    );
   }
 
   return (
-    <table className='tw-w-full tablet:tw-table-auto phone:tw-table-auto tw-border-collapse'>
-      <thead className='tw-sticky tw-top-0'>
-        <tr className='tw-bg-col-table-header tw-text-white'>
-          <th className='tw-text-center tw-py-1'>DATUM</th>
-          <th className='tw-text-center tw-py-1 phone:tw-hidden'>MODUS</th>
-          <th className='tw-text-center tw-py-1'>TEAMS</th>
-          <th className='tw-text-center tw-py-1'>SÄTZE</th>
-          <th colSpan={5} className='tw-text-left tw-py-1 phone:tw-hidden'>PUNKTE</th>
+    <table className="tw-w-full tablet:tw-table-auto phone:tw-table-auto tw-border-collapse">
+      <thead className="tw-sticky tw-top-0">
+        <tr className="tw-bg-col-table-header tw-text-white">
+          <th className="tw-text-center tw-py-1">DATUM</th>
+          <th className="tw-text-center tw-py-1 phone:tw-hidden">MODUS</th>
+          <th className="tw-text-center tw-py-1">TEAMS</th>
+          <th className="tw-text-center tw-py-1">SÄTZE</th>
+          <th colSpan={5} className="tw-text-left tw-py-1 phone:tw-hidden">
+            PUNKTE
+          </th>
         </tr>
       </thead>
-        {
-          results.results.map((result: ResultsSchema, index: number) => {
-            return (<TableRow key={index} {...result} maxPlayedSets={maxPlayedSets}/>)
-          })
-        }
+      {results.results.map((result: ResultsSchema, index: number) => {
+        return (
+          <TableRow key={index} {...result} maxPlayedSets={maxPlayedSets} />
+        );
+      })}
     </table>
   );
-}
+};
 
-const TableRow = (props: ResultsSchema & {maxPlayedSets: number}) => {
+const TableRow = (props: ResultsSchema & { maxPlayedSets: number }) => {
   const [result, setResult] = useState({} as ResultsSchema);
   const [shortDate, setShortDate] = useState('');
   const [loser, setLoser] = useState({} as ResultTeamSchema);
@@ -77,24 +91,54 @@ const TableRow = (props: ResultsSchema & {maxPlayedSets: number}) => {
     setWinnerSets(winnerSets.concat(fillArray));
     setLoserSets(loserSets.concat(fillArray));
 
-    const [, short,] = DateTransformer.transformDate(props.dateUtc);
+    const [, short] = DateTransformer.transformDate(props.dateUtc);
     setShortDate(short);
-  }, [])
+  }, []);
 
   return (
-    <tbody className='even:tw-bg-slate-50 hover:tw-bg-slate-100'>
+    <tbody className="even:tw-bg-slate-50 hover:tw-bg-slate-100">
       <tr>
-        <td rowSpan={2} className='tw-text-center tw-py-1'>{shortDate}</td>
-        <td rowSpan={2} className='tw-text-center tw-py-1 phone:tw-hidden'>{result.mode}</td>
-        <td className='tw-text-center tw-pt-1 tw-whitespace-nowrap tw-overflow-hidden tw-text-ellipsis'><strong>{winner.caption}</strong></td>
-        <td className='tw-text-center tw-pt-1'><strong>{winner.setsWon}</strong></td>
-        {winnerSets.map((set, index) => { return <td className='tw-text-left tw-pt-1 phone:tw-hidden' key={index}>{set > loser.sets[index] ? (<strong>{set}</strong>) : (<span className='tw-text-neutral-600'>{set}</span>)}</td> })}
+        <td rowSpan={2} className="tw-text-center tw-py-1">
+          {shortDate}
+        </td>
+        <td rowSpan={2} className="tw-text-center tw-py-1 phone:tw-hidden">
+          {result.mode}
+        </td>
+        <td className="tw-text-center tw-pt-1 tw-whitespace-nowrap tw-overflow-hidden tw-text-ellipsis">
+          <strong>{winner.caption}</strong>
+        </td>
+        <td className="tw-text-center tw-pt-1">
+          <strong>{winner.setsWon}</strong>
+        </td>
+        {winnerSets.map((set, index) => {
+          return (
+            <td className="tw-text-left tw-pt-1 phone:tw-hidden" key={index}>
+              {set > loser.sets[index] ? (
+                <strong>{set}</strong>
+              ) : (
+                <span className="tw-text-neutral-600">{set}</span>
+              )}
+            </td>
+          );
+        })}
       </tr>
-      <tr className='tw-border-0 tw-border-b-2 tw-border-solid tw-border-slate-200 hover:tw-bg-slate-100'>
-        <td className='tw-text-center tw-pb-1 tw-whitespace-nowrap tw-overflow-hidden tw-text-ellipsis'>{loser.caption}</td>
-        <td className='tw-text-center tw-pb-1'>{loser.setsWon}</td>
-        {loserSets.map((set, index) => { return <td className='tw-text-left tw-pt-1 phone:tw-hidden' key={index}>{set > winner.sets[index] ? (<strong>{set}</strong>) : (<span className='tw-text-neutral-600'>{set}</span>)}</td> })}
+      <tr className="tw-border-0 tw-border-b-2 tw-border-solid tw-border-slate-200 hover:tw-bg-slate-100">
+        <td className="tw-text-center tw-pb-1 tw-whitespace-nowrap tw-overflow-hidden tw-text-ellipsis">
+          {loser.caption}
+        </td>
+        <td className="tw-text-center tw-pb-1">{loser.setsWon}</td>
+        {loserSets.map((set, index) => {
+          return (
+            <td className="tw-text-left tw-pt-1 phone:tw-hidden" key={index}>
+              {set > winner.sets[index] ? (
+                <strong>{set}</strong>
+              ) : (
+                <span className="tw-text-neutral-600">{set}</span>
+              )}
+            </td>
+          );
+        })}
       </tr>
     </tbody>
-  )
-}
+  );
+};
