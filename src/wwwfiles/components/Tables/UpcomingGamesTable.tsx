7480948cb419555b33c6e18c623f9588d8b75439
Fabian Diemand <fabian.diemand@hotmail.com>
2024-10-04 23:04:36 +0200

    feature(logos): add logos to the upcoming games table

--- src/wwwfiles/components/Tables/UpcomingGamesTable.tsx
+++ src/wwwfiles/components/Tables/UpcomingGamesTable.tsx
@@ -1,38 +1,45 @@
 import { TeamProps } from '../../App';
-import { LocationSchema, UpcomingGamesSchema } from '../../common/types/UpcomingGamesByTeam.type';
+import {
+  LocationSchema,
+  UpcomingGamesSchema,
+} from '../../common/types/UpcomingGamesByTeam.type';
 import { Spinner } from '../Loading/Spinner';
 import { Toast } from '../Toast/Toast';
 import { GameType } from '../../common/enums/GameType.enum';
 import { useUpcomingGamesApi } from '../../common/hooks/api/useUpcomingGamesApi';
 import { useDateTransformer } from '../../common/hooks/transformers/useDateTransformer';
 import { useMapsLinkTransformer } from '../../common/hooks/transformers/useMapsLinkTransformer';
+import { TeamLogo } from '../Logo/TeamLogo';
 
-export const UpcomingGamesTable = ({teamId}: TeamProps) => {
+export const UpcomingGamesTable = ({ teamId }: TeamProps) => {
   const [loading, games, error] = useUpcomingGamesApi(teamId);
-  
+
   if (error) {
     console.error('Error while fetching upcoming games.');
-    return (
-      <Toast text="Beim Laden der Spiele ist ein Fehler aufgetreten." />
-    );
+    return <Toast text="Beim Laden der Spiele ist ein Fehler aufgetreten." />;
   }
 
   if (loading) {
-    return (<Spinner text='Lade nächste Spiele ...'/>);
+    return <Spinner text="Lade nächste Spiele ..." />;
   }
 
   if (!games.upcomingGames || games.upcomingGames.length === 0) {
-    return (<Toast text='Für deine Mannschaft sind keine weiteren Spiele vorhanden.'/>)
+    return (
+      <Toast text="Für deine Mannschaft sind keine weiteren Spiele vorhanden." />
+    );
   }
 
   return (
-    <table className="tw-w-full tablet:tw-table-fixed phone:tw-table-auto tw-border-collapse">
+    <table className="tw-w-full tablet:tw-table-fixed tw-table-auto phone:tw-table-auto tw-border-collapse">
       <thead className="tw-sticky tw-top-0">
         <tr className="tw-bg-col-gray tw-text-white">
-          <GamesTableHeader text="DATUM" />
-          <GamesTableHeader text="GEGNER" />
-          <GamesTableHeader text="TYP" mobileHidden={true} />
-          <GamesTableHeader text="ORT" />
+          <GamesTableHeader
+            text="DATUM"
+            styles="phone:tw-w-24 tw-text-center"
+          />
+          <GamesTableHeader text="GEGNER" styles="tablet:tw-text-center tw-text-left" />
+          <GamesTableHeader text="TYP" styles='tablet:tw-hidden tw-text-center'/>
+          <GamesTableHeader text="ORT" styles="phone:tw-w-20 tw-text-center" />
         </tr>
       </thead>
       <tbody>
@@ -42,24 +49,34 @@ export const UpcomingGamesTable = ({teamId}: TeamProps) => {
       </tbody>
     </table>
   );
-}
+};
 
 type GamesTableHeaderProps = {
-  text: string,
-  mobileHidden?: boolean
-}
+  text: string;
+  styles?: string
+};
 
-const GamesTableHeader = ({text, mobileHidden = false}: GamesTableHeaderProps) => {
-  return <th className={`tw-text-center tw-py-1 ${mobileHidden && 'phone:tw-hidden'}`}>{text}</th>;
-}
+const GamesTableHeader = ({
+  text,
+  styles
+}: GamesTableHeaderProps) => {
+  return (
+    <th
+      className={` tw-py-1 ${styles ?? ''}`}
+    >
+      {text}
+    </th>
+  );
+};
 
 type GamesTableRowProps = {
-  opponent: string,
-  mode: string,
-  type: GameType,
-  location: LocationSchema,
-  dateUtc: string
-}
+  opponent: string;
+  mode: string;
+  type: GameType;
+  location: LocationSchema;
+  dateUtc: string;
+  opponentLogoUrl: string;
+};
 
 const GamesTableRow = ({
   opponent,
@@ -67,49 +84,53 @@ const GamesTableRow = ({
   type,
   location,
   dateUtc,
+  opponentLogoUrl,
 }: GamesTableRowProps) => {
   const [long, short, time] = useDateTransformer(dateUtc);
+  const mapsLink = useMapsLinkTransformer(location.plusCode);
 
   return (
     <tr className="tw-border-0 tw-border-y-2 tw-border-solid tw-border-slate-200 tw-duration-200 hover:tw-bg-slate-100 even:tw-bg-slate-50 first:tw-border-t-0 last:tw-border-b-0">
+      <td className="tw-text-center tw-py-1">
+        <div className="tw-w-full tw-text-center tw-block tablet:tw-hidden">
+          {long}
+        </div>
+        <div className="tw-w-full tw-text-center tw-hidden tablet:tw-block">
+          {short}
+        </div>
+        {`${time} Uhr`}
+      </td>
+      <td className="tw-text-center tw-py-1 tw-whitespace-nowrap tw-overflow-hidden tw-text-ellipsis">
+        <div className="tablet:tw-h-12 tw-h-8 tw-flex tablet:tw-flex-col tw-flex-row tw-items-center phone:tw-gap-0 tw-gap-2">
+          <div className="tablet:tw-h-[50%] tw-h-full">
+            <TeamLogo src={opponentLogoUrl} />
+          </div>
+          {opponent}
+        </div>
+      </td>
       <td className="tw-text-center tw-py-1 tablet:tw-hidden">
-        {long}
-        <br />
-        {`${time} Uhr`}
-      </td>
-      <td className="tw-text-center tw-py-1 tablet:tw-table-cell tw-hidden">
-        {short}
-        <br />
-        {`${time} Uhr`}
-      </td>
-      <td className="tw-text-center tw-py-1 tablet:tw-hidden">{opponent}</td>
-      <td className="tw-text-center tw-py-1 tw-hidden tablet:tw-table-cell tw-whitespace-nowrap tw-overflow-hidden tw-text-ellipsis">
-        {opponent}
-      </td>
-      <td className="tw-text-center tw-py-1 phone:tw-hidden">
         {mode}
         <br />
         {type}
       </td>
       <td className="tw-text-center tw-py-1 tablet:tw-hidden tw-whitespace-nowrap  tw-overflow-hidden tw-text-ellipsis">
-        {`${location.caption} `}
-        <PinLink plusCode={location.plusCode} />
+        <a>{location.caption}</a>
+        <PinLink mapsLink={mapsLink} />
         <br />
-        {`${location.street} ${location.number}, ${location.zip} ${location.city}`}
+        <span className="tw-text-balance">{`${location.street} ${location.number}, ${location.zip} ${location.city}`}</span>
       </td>
       <td className="tw-text-center tw-py-1 tw-hidden tablet:tw-table-cell">
-        <PinLink plusCode={location.plusCode} />
+        <PinLink mapsLink={mapsLink} />
       </td>
     </tr>
   );
 };
 
 type PinLinkProps = {
-  plusCode: string
-}
+  mapsLink: string;
+};
 
-const PinLink = ({plusCode}: PinLinkProps) => {
-  const mapsLink = useMapsLinkTransformer(plusCode);
+const PinLink = ({ mapsLink }: PinLinkProps) => {
 
   return (
     <a href={mapsLink}>
@@ -132,4 +153,4 @@ const PinLink = ({plusCode}: PinLinkProps) => {
       </svg>
     </a>
   );
-}
+};
