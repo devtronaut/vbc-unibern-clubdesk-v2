b4a3614416698872c57cbda4b99a145929291579
Fabian Diemand <fabian.diemand@hotmail.com>
2024-10-04 10:45:55 +0200

    fix(button): call handler correctly to make nav work in firefox

--- src/wwwfiles/components/Controls/TableSwitchButton.tsx
+++ src/wwwfiles/components/Controls/TableSwitchButton.tsx
@@ -9,7 +9,7 @@ export const TableSwitchButton = ({
 }: TableSwitchButtonProps) => {
   return (
     <button
-      onClick={() => handler}
+      onClick={() => handler()}
       className="tw-bg-white tw-px-4 tw-py-3 tw-text-col-button tw-border-solid tw-border-2 tw-border-col-button tw-rounded-full tw-duration-500 hover:tw-bg-col-button hover:tw-text-white"
     >
       {text}
