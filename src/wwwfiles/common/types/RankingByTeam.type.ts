62422391e546a0b915ea9c25de51a229515283b6
Fabian Diemand <fabian.diemand@hotmail.com>
2024-10-04 15:45:01 +0200

    feature(logo): include logo in correct type

--- src/wwwfiles/common/types/RankingByTeam.type.ts
+++ src/wwwfiles/common/types/RankingByTeam.type.ts
@@ -15,6 +15,7 @@ type RankDetailsSchema = {
 export type TeamRankingSchema = {
   rank: number,
   teamCaption: string,
+  teamLogoUrl: string,
   wins: number,
   defeats: number,
   points: number,
@@ -25,7 +26,6 @@ export type RankingSchema = {
   id: string,
   teamId: number,
   leagueCaption: string,
-  teamLogoUrl: string
   teams: TeamRankingSchema[],
   createdAt: string
 }
