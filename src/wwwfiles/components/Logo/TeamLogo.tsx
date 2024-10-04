0c3f17e6376ba7166ec68972a66ab38debbab8a0
Fabian Diemand <fabian.diemand@hotmail.com>
2024-10-04 20:50:48 +0200

    feature(logos): fix display width for logos

--- src/wwwfiles/components/Logo/TeamLogo.tsx
+++ src/wwwfiles/components/Logo/TeamLogo.tsx
@@ -1,22 +1,20 @@
 import { VolleyballIcon } from './VolleyballIcon';
 
 type TeamLogoProps = {
-    src?: string
-}
+  src?: string;
+};
 
-export const TeamLogo = ({src}: TeamLogoProps) => {
-    return (
-      <>
-        <div className="tw-h-full ">
-          {src ? (
-            <img
-              src={src}
-              className="tw-h-full tw-rounded-full"
-            />
-          ) : (
-            <VolleyballIcon />
-          )}
-        </div>
-      </>
-    );
-}
+export const TeamLogo = ({ src }: TeamLogoProps) => {
+  return (
+    <div className="tw-h-full tw-w-fit tw-min-w-fit">
+      {src ? (
+        <img
+          src={src}
+          className="tw-h-full tw-w-fit tw-min-w-fit tw-min-h-full tw-rounded-full"
+        />
+      ) : (
+        <VolleyballIcon />
+      )}
+    </div>
+  );
+};
