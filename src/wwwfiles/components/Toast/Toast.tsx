581a373f52123b34d62aa6945b42676adfabf708
Fabian Diemand <fabian.diemand@hotmail.com>
2024-10-24 14:37:18 +0200

    fix(error-toast): add margin, remove fixed width

--- src/wwwfiles/components/Toast/Toast.tsx
+++ src/wwwfiles/components/Toast/Toast.tsx
@@ -5,7 +5,7 @@ type ToastProps = {
 export const Toast = ({text}: ToastProps) => {
   return (
     <div className='tw-h-full tw-flex tw-flex-col tw-items-center tw-justify-center tw-space-y-5 tw-min-h-[300px] '>
-      <div className='tw-flex tw-items-center tw-w-full tw-max-w-xs tw-p-4 tw-space-x-4 tw-bg-col-gray tw-rounded-lg tw-shadow'>
+      <div className='tw-flex tw-items-center tw-max-w-xs tw-p-4 tw-m-2 tw-space-x-4 tw-bg-col-gray tw-rounded-lg tw-shadow'>
         <div className='tw-pl-4 !tw-text-white tw-text-center'>{text}</div>
       </div>
     </div>
